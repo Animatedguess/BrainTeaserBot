a7c235858060b886221e4784818b318da97d6a84
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2024-09-15 23:36:41 +0530

    Update LandingCom.js

--- src/component/LandingCom.js
+++ src/component/LandingCom.js
@@ -66,12 +66,12 @@ function LandingCom() {
                                         value={inputValue}
                                         onChange={handleChange}
                                         type="text"
-                                        className="px-4 w-3/4 py-2 border rounded-md bg-gray-800 text-gray-300 dark:border-gray-600 focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                                        className="px-4 w-full lg:w-3/4 py-2 border rounded-md bg-gray-800 text-gray-300 dark:border-gray-600 focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
                                         placeholder="Ask your question here..."
                                     />
                                     <button
                                         onClick={() => setButtonClicked(!buttonClicked)}
-                                        className="w-1/4 px-5 py-2 text-sm tracking-wider text-white transition-colors duration-300 transform bg-landing-color rounded-lg lg:w-auto lg:mx-4 hover:bg-gray-400 focus:outline-none focus:bg-landing-color"
+                                        className="w-full lg:w-auto px-5 py-2 text-sm tracking-wider text-white transition-colors duration-300 transform bg-landing-color rounded-lg lg:mx-4 hover:bg-gray-400 focus:outline-none focus:bg-landing-color"
                                     >
                                         Ask AI
                                     </button>
@@ -80,7 +80,7 @@ function LandingCom() {
                         </div>
                         <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
                             <img
-                                className="w-full h-full max-w-md pointer-events-none"
+                                className="w-3/4 lg:w-full h-auto max-w-md pointer-events-none"
                                 src={`${process.env.PUBLIC_URL}/landingPage-Main-Image.png`}
                                 alt="Email illustration vector art"
                             />

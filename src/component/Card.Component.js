d783a482b4e8ed26263f4ba51ece5ec7f2e54010
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2024-09-16 01:31:17 +0530

    Update Card.Component.js

--- src/component/Card.Component.js
+++ src/component/Card.Component.js
@@ -5,22 +5,22 @@ function Card(promps) {
     return (
         <Link to={promps.name}>
             <div className='bg-quiz-color rounded-lg shadow-lg'>
-            <div className="w-full max-w-xs overflow-hidden" tabIndex="0">
-                <div className="flex justify-center items-center">
-                    <img
-                        className="object-cover w-56 h-56 shadow-2xl rounded-lg"
-                        src={promps.path}
-                        alt="avatar"
-                    />
-                </div>
+                <div className="w-full max-w-xs overflow-hidden" tabIndex="0">
+                    <div className="flex justify-center items-center">
+                        <img
+                            className="object-cover w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 shadow-2xl rounded-lg"
+                            src={promps.path}
+                            alt="avatar"
+                        />
+                    </div>
 
-                <div className="py-5 text-center">
-                    <p className="block text-xl font-bold text-white bg-[#CCAC8E]">
-                        {promps.name}
-                    </p>
+                    <div className="py-5 text-center">
+                        <p className="block text-lg sm:text-xl md:text-2xl font-bold text-white bg-[#CCAC8E] px-2 py-1">
+                            {promps.name}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
         </Link>
     )
 }
